04bf11456a08268a4c0485fc2aa12d586a90f10e
Kayeon Do <dodo1305@naver.com>
2018-06-21 15:44:04 +0900

    XML 스키마 기반으로 AOP 구현

--- src/main/java/kr/hs/emirim/dodo1305/aopcal/cal/ImpCalculator.java
+++ src/main/java/kr/hs/emirim/dodo1305/aopcal/cal/ImpCalculator.java
@@ -5,7 +5,7 @@ public class ImpCalculator implements Calculator{
 	public long factorial(long num) {
 		long result = 1;
 		for(long i = 1; i <=num; i++) {
-			result *= 1;
+			result *= i;
 		}
 		return result;
 	} 
